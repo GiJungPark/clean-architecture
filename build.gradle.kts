838b025ea6631db204bff8325955ce7111656ca3
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-28 23:21:12 +0900

    chore: Lombok 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,10 +29,10 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
